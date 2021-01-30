26b13b347a72b10fc298568654af6650e12cbd8d
Thiago Ferreira <thiaago.dev@gmail.com>
2021-01-30 15:02:31 -0300

    Criando model e migration de Tasks

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    description = models.CharField(max_length=255, null=False)
+    created_at = models.DateField(auto_now_add=True)
+    task_finish = models.BooleanField(default=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
