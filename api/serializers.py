a001260543c1f891a7d8be629f6412714e5affeb
Thiago Ferreira <thiaago.dev@gmail.com>
2021-02-11 17:00:38 -0300

    Alterando model de User

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
 from .models import *
 
 
--- tasks_api/settings.py
+++ tasks_api/settings.py
@@ -30,6 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'api.User'
 
 # Application definition
 
