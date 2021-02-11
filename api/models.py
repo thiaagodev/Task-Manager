95f65ec7eb3a03f8ba1eb9898bd490b07fa3abba
Thiago Ferreira <thiaago.dev@gmail.com>
2021-02-11 16:50:54 -0300

    Criando model UserManager

--- api/models.py
+++ api/models.py
@@ -1,7 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    
+    def create_user(self, username, first_name, last_name, email, password):
+        if username is None:
+            raise TypeError('Usuário deve ter username')
+        if username is None:
+                raise TypeError('Usuário deve ter email')
+
+        user = self.model(username=username, emial=self.normalize_email(email), first_name=first_name, last_name = last_name)
+        user.set_password(password)
+        user.is_superuser = False
+        user.is_staff = False
+        user.save()
+
+
+
+
 
 class Task(models.Model):
     title = models.CharField(max_length=255, null=False)
